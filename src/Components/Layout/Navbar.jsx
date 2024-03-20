281d0ba9e298f42ac76240d74bd4055c9c514202
shyam-tvr <shyammtvr@gmail.com>
2024-03-20 21:35:14 +0530

    some changes in navbar

--- src/Components/Layout/Navbar.jsx
+++ src/Components/Layout/Navbar.jsx
@@ -40,13 +40,13 @@ function Navbar() {
 
 
                     <ul className={`md:flex md:items-center md:pb-0 absolute md:static opacity-0.8 md:z-auto left-0 w-full md:w-auto md:pl-0 pl-4 bg-black bg-opacity-20 md:bg-black  border-t border-indigo-600  md:border-none ${open ? 'z-1 relative opacity-0.8  ' : ' md:opacity-100 opacity-0'}`}>
-                        <li className={`md:ml-8 font-base md:my-0 cursor-pointer my-4   ${window.location.pathname == '/' ? 'text-blue-500' : 'text-gray-200'}  duration-500`} onClick={() => navigate('/')} > HOME </li>
-                        <li className={`md:ml-8 font-base md:my-0  cursor-pointer  my-4 ${window.location.pathname == '/services' ? 'text-blue-500' : 'text-gray-200'} duration-500`} onClick={() => navigate('/services')}>
+                        <li className={`md:ml-8 font-base md:my-0 cursor-pointer my-4   ${window.location.pathname == '/' ? 'text-blue-500' : 'text-gray-200'}  duration-500`} onClick={() => {navigate('/');handleToggle()}} > HOME </li>
+                        <li className={`md:ml-8 font-base md:my-0  cursor-pointer  my-4 ${window.location.pathname == '/services' ? 'text-blue-500' : 'text-gray-200'} duration-500`} onClick={() => {navigate('/services');handleToggle()}}>
                             SERVICES
                         </li>
-                        <li className={`md:ml-8 font-base md:my-0 cursor-pointer  my-4 ${window.location.pathname == '/about' ? 'text-blue-500' : 'text-gray-200'} duration-500`} onClick={() => navigate('/about')} > ABOUT</li>
-                        <li className={`md:ml-8 font-base md:my-0 cursor-pointer  my-4 ${window.location.pathname == '/career' ? 'text-blue-500' : 'text-gray-200'}`} onClick={() => navigate('/career')} > CAREERS</li>
-                        <li className={`md:ml-8 font-base md:my-0 cursor-pointer pb-4 md:pb-0  my-4 ${window.location.pathname == '/contact' ? 'text-blue-500' : 'text-gray-200'}`} onClick={() => navigate('/contact')} > CONTACT</li>
+                        <li className={`md:ml-8 font-base md:my-0 cursor-pointer  my-4 ${window.location.pathname == '/about' ? 'text-blue-500' : 'text-gray-200'} duration-500`} onClick={() => {navigate('/about');handleToggle()}} > ABOUT</li>
+                        <li className={`md:ml-8 font-base md:my-0 cursor-pointer  my-4 ${window.location.pathname == '/career' ? 'text-blue-500' : 'text-gray-200'}`} onClick={() => {navigate('/career');handleToggle()}} > CAREERS</li>
+                        <li className={`md:ml-8 font-base md:my-0 cursor-pointer pb-4 md:pb-0  my-4 ${window.location.pathname == '/contact' ? 'text-blue-500' : 'text-gray-200'}`} onClick={() =>{navigate('/contact');handleToggle()}} > CONTACT</li>
                     </ul>
                 </div>
             </div>
--- src/Utils/Routes.js
+++ src/Utils/Routes.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "../Pages/Layout";
 import Home from "../Pages/Home";
 import Services from "../Pages/Services";
@@ -11,6 +11,7 @@ const Routers = () => {
         <Routes>
             <Route element={<Layout />}>
                 <Route exact path="/" element={<Home />}/>
+                <Route path="/lander" element={<Navigate to="/" replace />} />
                 <Route path="/services" element = {<Services />} />
                 <Route path="/about" element = {<About />} />
                 <Route path="/career" element = {<Career />} />
